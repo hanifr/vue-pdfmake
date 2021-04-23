6f3d4486569707f2715334f1e7cb7ed5cfde9cbb
hanifr <irhaniframli@gmail.com>
2021-04-24 07:48:04 +0800

    update main files

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,96 @@
+<template>
+<!-- en el div debemos poner una referencia -->
+  <div class="hello" ref="capture">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <h1>{{ msg }}</h1>
+    <p>
+      For a guide and recipes on how to configure / customize this project,<br>
+      check out the
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+    </p>
+    <h3>Installed CLI Plugins</h3>
+    <ul>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+    </ul>
+    <h3>Essential Links</h3>
+    <ul>
+      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
+      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
+      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
+      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
+      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
+    </ul>
+    <h3>Ecosystem</h3>
+    <ul>
+      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
+      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
+      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
+      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
+      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+    </ul>
+    <!-- boton para imprimir el contenido del div -->
+    <button @click="downloadHelloWorld">Print HelloWorld</button>
+  </div>
+</template>
+
+<script>
+// importamos html2canvas
+import html2canvas from 'html2canvas'
+// inportamos pdfmake
+var pdfMake = require('pdfmake/build/pdfmake.js');
+var pdfFonts = require('pdfmake/build/vfs_fonts.js');
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+    data() {
+    return {
+      pdf: null
+    }
+  },
+  methods: {
+    downloadHelloWorld () {
+      let vc = this
+      // capturamos el div con html2canvas para despues descargarlo con pdfmake
+      html2canvas(vc.$refs.capture).then(canvas => {
+          var data = canvas.toDataURL();
+                var docDefinition = {
+                    content: [{
+                        image: data,
+                        width: 500,
+                    }],
+                    pageSize: 'A4',
+                };
+                pdfMake.createPdf(docDefinition).download("helloworld.pdf");
+      }).catch((error) => {
+        // eslint-disable-next-line no-console
+        console.log("Erorr descargando Hello World", error)
+      });
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.hello{
+  width: 612px;
+}
+</style>
